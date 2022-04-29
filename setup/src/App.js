d0b8cdb2031f85e0bc831f8c365aa393e53c7d55
Dino Hadzic <zgdino@yahoo.com>
2022-04-29 10:20:58 +0200

    landing title

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-  return <h2>pagination starter</h2>
+  const {loading, data} = useFetch()
+  
+  return <main>
+    <div className="section-title">
+      <h1>{loading ? 'loading...' : 'pagination'}</h1>
+      <div className="underline"></div>
+    </div>
+  </main>
 }
 
 export default App
