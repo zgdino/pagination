4d418b5bdf2e86d5562604117542ca57687055a8
Dino Hadzic <zgdino@yahoo.com>
2022-05-04 12:47:06 +0200

    array of array

--- setup/src/utils.js
+++ setup/src/utils.js
@@ -1,8 +1,14 @@
 const paginate = (followers) => {
- const itemsPerPage = 10
- // using Math.ceil to round up the number
- const pages = Math.ceil(followers.length / itemsPerPage)
- console.log(pages);
+  const itemsPerPage = 10
+  // using Math.ceil to round up the number
+  const pages = Math.ceil(followers.length / itemsPerPage)
+  // Array.from is allowing us to create a new array from other types of data
+  // setting up the length of newFollowers to be equal to followers
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+   const start = index * itemsPerPage
+   // pulling it from followers from start to start + itemsPerPage
+   return followers.slice(start, start + itemsPerPage)
+  })
 }
 
 export default paginate
